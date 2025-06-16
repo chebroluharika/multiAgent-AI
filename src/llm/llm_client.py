5e0051e5a2551edb3669365e615a47920cede074
Chebrolu Bala Sai Harika <34155580+chebroluharika@users.noreply.github.com>
2025-06-16 15:25:09 +0530

    Create llm_client.py

---
+++ src/llm/llm_client.py
@@ -0,0 +1,51 @@
+import os
+
+from crewai import LLM
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def openai_llm_client(model: str = "openai/gpt-4o-mini"):
+    assert model.startswith("openai/"), "Model must start with 'openai/'"
+    return LLM(
+        model=model,
+        api_key=os.getenv("OPENAI_API_KEY"),
+    )
+
+
+def groq_llm_client(model: str = "groq/llama3-70b-8192"):
+    assert model.startswith("groq/"), "Model must start with 'groq/'"
+    return LLM(
+        model=model,
+        api_key=os.getenv("GROQ_API_KEY"),
+    )
+
+
+def hf_llm_client(model: str = "huggingface/mistralai/Mistral-7B-Instruct-v0.3"):
+    assert model.startswith("huggingface/"), "Model must start with 'huggingface/'"
+
+    return LLM(
+        model=model,
+        api_key=os.getenv("HF_TOKEN"),
+    )
+
+
+def gemini_llm_client(model: str = "gemini/gemini-2.0-flash-lite"):
+    assert model.startswith("gemini/"), "Model must start with 'gemini/'"
+    return LLM(
+        model=model,
+        api_key=os.getenv("GEMINI_API_KEY"),
+    )
+
+
+if __name__ == "__main__":
+    print(os.getenv("OPENAI_API_KEY"))
+    openai_llm = openai_llm_client("openai/gpt-4o-mini")
+    print(openai_llm.call("Hello, how are you?"))
+
+    # groq_llm = groq_llm_client("groq/llama3-70b-8192")
+    # print(groq_llm.call("Hello, how are you?"))
+
+    # hf_llm = hf_llm_client("huggingface/mistralai/Mistral-7B-Instruct-v0.3")
+    # print(hf_llm.call("Hello, how are you?"))
